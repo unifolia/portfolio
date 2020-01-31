182c2911127c405bde12f127a726de7af4fbaa28
unifolia <james@electronicmail.ca>
2020-01-31 02:57:45 -0500

    Removed Slick(), added James graphic, changed background colour, worked on element positioning, optimized jQuery code, removed feature that were no longer needed

--- javascript.js
+++ javascript.js
@@ -1,74 +1,154 @@
-$(function() {
+const portfolio = {}
 
-    let myInterests = ['🐱', '🏀', '🎸', '🌺', '🍺', '💻'];
-    let greetingsArray = ["Hello", "Dearest", "Hi", "Hey", "Greetings"];
-    let userInput = '';
-    let konamiKey = '38384040373937396665';
+let userInput = ''
+let skiInput = ''
+let konamiKey = '38384040373937396665'
+let skiKey = '766984837179837573737871'
+let greetingsArray = ["Hello", "Dearest", "Hi", "Hey", "Greetings", "Good day", "Salutations"]
+let myInterests = ['🐱', '🏀', '🎸', '🌺', '🍺', '💻']
+let skiArray = [
+    "⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤🌲⠤⠤⠤⠤🌲⠤⠤⠤⠤⠤",
+    "🌲⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤🌲⠤⠤⠤⠤🌲⠤⠤🏂",
+    "⠤🌲⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤🌲⠤⠤⠤⠤🌲🏂⠤",
+    "⠤⠤🌲⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤🌲⠤⠤⠤⠤🏂⠤⠤",
+    "⠤⠤⠤🌲⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤🌲⠤⠤🏂🌲⠤",
+    "🌲⠤⠤🌲⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤🌲🏂⠤⠤🌲",
+    "⠤🌲⠤⠤🌲⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤🌲⠤⠤⠤⠤⠤",
+    "⠤⠤🌲⠤⠤🌲⠤⠤⠤⠤⠤⠤⠤⠤🏂🌲⠤⠤⠤⠤",
+    "⠤⠤⠤🌲⠤⠤🌲⠤⠤⠤⠤⠤⠤🏂⠤⠤🌲⠤⠤⠤",
+    "⠤⠤⠤⠤🌲⠤⠤🌲⠤⠤⠤⠤🏂⠤⠤⠤⠤🌲⠤⠤",
+    "⠤⠤⠤⠤⠤🌲⠤⠤🌲⠤⠤🏂⠤⠤⠤⠤⠤⠤🌲⠤",
+    "⠤⠤⠤⠤⠤⠤🌲⠤⠤🌲🏂⠤⠤⠤⠤⠤⠤⠤⠤🌲",
+    "⠤⠤⠤⠤⠤⠤⠤🌲⠤⠤🏂⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤",
+    "⠤⠤⠤⠤⠤⠤⠤⠤🌲🏂🌲⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤",
+    "⠤⠤⠤⠤⠤⠤⠤⠤⠤🌲⠤⠤🌲⠤⠤⠤⠤⠤⠤⠤⠤⠤",
+    "⠤⠤⠤⠤⠤⠤⠤⠤🏂🌲⠤⠤🌲⠤⠤⠤⠤⠤⠤⠤⠤",
+    "⠤⠤⠤⠤⠤⠤⠤🏂⠤⠤🌲⠤⠤🌲⠤⠤⠤⠤⠤⠤⠤",
+    "🌲⠤⠤⠤⠤🏂⠤⠤⠤⠤🌲⠤⠤🌲⠤⠤⠤⠤⠤⠤",
+    "⠤🌲⠤⠤🏂⠤⠤⠤⠤⠤⠤🌲⠤⠤🌲⠤⠤⠤⠤⠤",
+    "⠤⠤🌲🏂⠤⠤⠤⠤⠤⠤⠤⠤🌲⠤⠤🌲⠤⠤⠤⠤",
+    "⠤⠤⠤🏂⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤🌲⠤⠤🌲⠤⠤⠤",
+    "⠤⠤🏂🌲⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤🌲⠤⠤🌲⠤⠤",
+    "⠤🏂⠤⠤🌲⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤🌲⠤⠤🌲⠤",
+    "🌲⠤⠤⠤⠤🌲⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤🌲⠤⠤🌲",
+    "⠤🌲⠤⠤⠤⠤🌲⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤🌲⠤⠤⠤",
+    "⠤⠤🌲⠤⠤⠤⠤🌲⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤🌲⠤⠤",
+    "⠤⠤⠤🌲⠤⠤⠤⠤🌲⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤🌲⠤",
+    "⠤⠤⠤⠤🌲⠤⠤⠤⠤🌲⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤🌲",
+    "⠤⠤⠤⠤⠤🌲⠤⠤⠤⠤🌲⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤",
+    "⠤⠤⠤⠤⠤⠤🌲⠤⠤⠤⠤🌲⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤",
+    "⠤⠤⠤⠤⠤⠤⠤🌲⠤⠤⠤⠤🌲⠤⠤⠤⠤⠤⠤⠤⠤⠤",
+    "⠤⠤⠤⠤⠤⠤⠤⠤🌲⠤⠤⠤⠤🌲⠤⠤⠤⠤⠤⠤⠤⠤",
+    "⠤⠤⠤⠤⠤⠤⠤⠤⠤🌲⠤⠤⠤⠤🌲⠤⠤⠤⠤⠤⠤⠤",
+    "⠤⠤⠤⠤⠤⠤⠤⠤⠤⠤🌲⠤⠤⠤⠤🌲⠤⠤⠤⠤⠤⠤"
+]
 
-    loopGreetings = (greetingsArray) => {
-        randomGreeting = greetingsArray[Math.floor(Math.random() * greetingsArray.length)];
+portfolio.loopGreetings = (greetingsArray) => {
+    randomGreeting = greetingsArray[Math.floor(Math.random() * greetingsArray.length)]
 
-        $('textarea').attr("placeholder", `${randomGreeting} James, `);
-    };
+    $('textarea').attr("placeholder", `${randomGreeting} James, `)
+}
 
-    loopGreetings(greetingsArray);
+portfolio.onKonamiCode = (callback) => {
+    document.addEventListener('keydown', function(e) {
+        userInput += ("" + e.keyCode)
+        if (userInput === konamiKey) {
+            return callback();
+        } else if (!konamiKey.indexOf(userInput)) {
+            return 
+        } else {
+            userInput = ("" + e.keyCode)
+        }
+    })
+}
 
-    onKonamiCode = callback => {
-        document.addEventListener('keydown', function(e) {
-            userInput += ("" + e.keyCode);
-            if (userInput === konamiKey) {
-                return callback();
-            } else if (!konamiKey.indexOf(userInput)) {
-                return 
-            } else {
-                userInput = ("" + e.keyCode);
-            };
-        });
-    };
 
-    onKonamiCode(function() {
-        $('.headshot').addClass("cursorPointer");
-        $('.headshot').on("click", function() {
-            funEmojiTime();
-        });
-    });
+portfolio.funEmojiTime = () => {
+    $('.headshot').removeClass("cursorPointer");
+    commenceLoop = () => {
+        let randomEmoji = myInterests[Math.floor((Date.now() / 400) % myInterests.length)]
 
-    funEmojiTime = () => {
-        $('.headshot').removeClass("cursorPointer");
-        commenceLoop = () => {
-            randomEmoji = myInterests[Math.floor((Date.now() / 400) % myInterests.length)];
+        $('h1').html(`I enjoy: ${randomEmoji}`)
+        
+        setTimeout(commenceLoop, 50);
+    }
+    commenceLoop()
+}
 
-            $('h1').html(`I enjoy: ${randomEmoji}`)
-            history.replaceState({ page: 1 }, "title 1", `?${randomEmoji}`);
-            history.go(1);
-            
-            setTimeout(commenceLoop, 50);
-        };
-        commenceLoop();
-    };
+portfolio.onSkiCode = (callback) => {
+    document.addEventListener('keydown', function (e) {
+        skiInput += ("" + e.keyCode)
+        if (skiInput === skiKey) {
+            return callback();
+        } else if (!skiKey.indexOf(skiInput)) {
+            return
+        } else {
+            skiInput = ("" + e.keyCode)
+        }
+    })
+}
 
-    $('.scrollDownButton').on("click", function(){
+
+portfolio.skiEmojiTime = () => {
+    skiEmojiTimeLoop = () => {
+        location.hash = skiArray[Math.floor((Date.now() / 120) % skiArray.length)]
+    
+        setTimeout(portfolio.skiEmojiTime, 50);
+    }
+    skiEmojiTimeLoop()
+}
+
+portfolio.SmoothScroll = () => {
+    $('.scrollToProjects').on("click", function () {
         $(`html,body`).animate({
             scrollTop: $(`#projects`).offset().top
-        }, 300, `linear`);
-    });
+        }, 250, `linear`)
+    })
 
-    // 
-    // $('.toGrid').on("click", function(){
-    //     $('.headerContent').css("display", "grid");
-    // });
+    $('.scrollToSkills').on("click", function () {
+        $(`html,body`).animate({
+            scrollTop: $(`#skills`).offset().top
+        }, 0, `linear`)
+    })
+}
 
-    // $('.toFlexbox').on("click", function () {
-    //     $('header, .headerContent').css("display", "flex");
-    // });
 
-    // $('.toFloat').on("click", function() {
-    //     $('.headerContent').css("display", "block");
-    //     $('header img').css("float", "left");
-    //     $('header section').css("float", "right");
-    // });
+portfolio.LightToggle = () => {
+    $('.checkbox').on("click", function(){
+        $('body').toggleClass("lightsOut")
+        if ($('body').hasClass("lightsOut") === true) {
+            $('.jamesCameraRoll img').attr("src", "./assets/jamesCouchNight.png")
 
-    // $('.toGrid, .toFloat').on("click", function(){
-    //     $('header').css("display", "block");
-    // });
-});
+            portfolio.onSkiCode(function () {
+                portfolio.skiEmojiTime()
+            })
+        } else {
+            $('.jamesCameraRoll img').attr("src", "./assets/jamesCouch.png")
+        }
+    })
+}
+
+portfolio.conditionalFadeRight = () => {
+    if ($(window).width() < 940) {
+        $('div.projectTwo').attr("data-aos", "fade-right")
+    }
+}
+
+    
+portfolio.init = () => {
+    portfolio.conditionalFadeRight()
+    portfolio.loopGreetings(greetingsArray)
+    AOS.init()
+    portfolio.SmoothScroll()
+    portfolio.LightToggle()
+    portfolio.onKonamiCode(function() {
+        $('.headshot').addClass("cursorPointer");
+        $('.headshot.cursorPointer').on("click", function() {
+            portfolio.funEmojiTime()
+        })
+    })
+}
+
+$(function() {
+    portfolio.init()
+})
